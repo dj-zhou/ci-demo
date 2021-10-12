1ee4c563fffc70f07b8cd50020bafb8050bc816e
Dingjiang Zhou <zhoudingjiang@gmail.com>
2021-10-12 13:53:33 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+File Name     : main.cpp
+Author        : Dingjiang Zhou
+Email         : zhoudingjiang@gmail.com
+Create Time   : Tue, Oct. 12th, 2021. 01:52:26 PM
+Last Modified :
+Purpose       :
+-------------------------------------------------------------------------------
+INPUTS
+
+OUTPUTS
+
+******************************************************************************/
+#include <iostream>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    ( void )argc;
+    ( void )argv;
+    for (;;) {
+        printf("hello world\n");
+        sleep(1);
+    }
+    return 0;
+}
