0cbfdf4be06700af27af8451facd0cca7bd78bac
Dingjiang Zhou <zhoudingjiang@gmail.com>
2021-11-06 22:35:02 -0400

    add no-format/ directory for CI test

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,5 @@
-/******************************************************************************
-File Name     : main.cpp
-Author        : Dingjiang Zhou
-Email         : zhoudingjiang@gmail.com
-Create Time   : Tue, Oct. 12th, 2021. 01:52:26 PM
-Last Modified :
-Purpose       :
--------------------------------------------------------------------------------
-INPUTS
 
-OUTPUTS
-
-******************************************************************************/
+#include "no-format.h"
 #include <iostream>
 #include <math.h>
 #include <stdint.h>
@@ -24,6 +13,8 @@ int main(int argc, char* argv[]) {
     ( void )argc;
     ( void )argv;
     static int loop = 0;
+    function1();
+    function2(10);
     for (;;) {
         printf("%d: hello world\n", loop++);
         sleep(1);
